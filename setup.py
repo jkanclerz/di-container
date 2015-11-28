3291cd2678ddaf699f16859f6b773a1838e99ab6
Jakub Kanclerz <kuba.kanclerz@creativestyle.pl>
2015-11-28 18:28:30 +0100

    change name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.rst', 'r') as readme:
     long_description = readme.read()
 
 setup(
-    name='di container',
+    name='dicontainer',
     version='0.1.0',
     description='',
     long_description=long_description,
     author='Jakub Kanclera',
     author_email='jakub.kanclerz@gmail.com',
     url='',
-    py_modules=['di_container'],
+    py_modules=['dicontainer'],
     license='MIT',
     keywords=[],
     test_suite='tests'
